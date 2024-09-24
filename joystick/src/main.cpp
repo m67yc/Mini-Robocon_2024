a5ba14f57b1324e289df64d15a37025a3861800f
m67yc <martinmcymay@gmail.com>
2024-09-24 17:48:53 +0800

    Add files via upload

---
+++ joystick/src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+#include "Joystick_309.h"
+
+// Consider the function should return the value or not
+
+
+Joystick joystick(A1,A2);
+
+void setup() {
+
+}
+
+void loop(){
+    
+    // joystick.setDeadZoneX();
+    // joystick.setDeadZoneY();
+    // joystick.showRawValue();
+    // joystick.showMapValue();
+    // Serial.println();
+
+}
