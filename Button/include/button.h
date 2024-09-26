8e1fec88331b47b26e53c65896788a587a83bb1f
m67yc <martinmcymay@gmail.com>
2024-09-26 17:43:27 +0800

    Add files via upload

--- Button/include/button.h
+++ Button/include/button.h
@@ -6,11 +6,16 @@ class Button{
     private:
         const uint8_t m_pin_button;
 
+        uint16_t m_speed;
+        uint8_t m_direction;
+        uint8_t m_mode;
+
         
     public:
         Button(const uint8_t pin_button);
         bool readValue();
-        uint16_t getSpeedValue(uint16_t speed, uint8_t mode);
+        uint16_t setSpeedValue(uint16_t speed, uint8_t mode);
+        uint8_t setDirectionValue(uint8_t direction, uint8_t mode);
         //...
 
 };
--- Button/src/button.cpp
+++ Button/src/button.cpp
@@ -12,3 +12,33 @@ bool Button::readValue(){
     return digitalRead(m_pin_button);
 
 }
+
+uint16_t setSpeedValue(uint16_t speed, uint8_t mode){
+
+    m_speed = speed;
+    m_mode = mode;
+    if(mode == 2){
+        m_speed += 50;
+    }
+    else if(mode == 0){
+        m_speed -= 50;
+    }
+
+    return m_speed;
+
+}
+
+uint8_t setDirectionValue(uint8_t direction, uint8_t mode){
+
+    m_direction = direction;
+    m_mode = mode;
+    if(mode == 2){
+        m_direction += 1;
+    }
+    else if(mode == 0){
+        m_direction -= 1;
+    }
+
+    return m_direction;
+
+}
